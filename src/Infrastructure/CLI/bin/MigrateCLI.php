c68b2a711b71dcb83647d3a797087ffa6c4a7b73
Iago <prog.iago.oliveira@gmail.com>
2025-06-15 01:32:20 -0300

    migrate cli

---
+++ src/Infrastructure/CLI/bin/MigrateCLI.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Framework\Infrastructure\CLI\bin;
+
+use Framework\Core\Main;
+use Framework\Infrastructure\DB\Migrations\PdoMigrationManager;
+
+/**
+ * @author Iago Oliveira <prog.iago.oliveira@gmail.com>
+ * @since 15/06/2025
+ */
+class MigrateCLI
+{
+    protected $migrationsDir;
+    protected $command;
+    protected $manager;
+
+    public function __construct(string $migrationsDir)
+    {
+        $this->setMigrationsDir($migrationsDir);
+        $this->validaArgs();
+        $this->setCommand();
+    }
+
+    protected  function setMigrationsDir(string $migrationsDir)
+    {
+        $this->migrationsDir = $migrationsDir;
+    }
+
+    protected function validaArgs()
+    {
+        global $argc;
+
+        if ($argc < 2) {
+            echo "Uso: migrate <comando> [argumentos]\n";
+            echo "Comandos disponíveis:\n";
+            echo "  run               Executa todas as migrations pendentes.\n";
+            echo "  rollback          Reverte a última migration executada.\n";
+            echo "  up <migration>    Executa o 'up' de uma migration específica.\n";
+            echo "  down <migration>  Executa o 'down' de uma migration específica.\n";
+            exit(1);
+        }
+    }
+
+    protected function setCommand()
+    {
+        global $argv;
+        $this->command = $argv[1];
+    }
+
+    protected function execute()
+    {
+        global $argv, $argc;
+
+        switch ($this->command) {
+            case 'run':
+                $this->getManager()->runMigrations();
+                break;
+
+            case 'rollback':
+                $steps = isset($argv[2]) ? (int)$argv[2] : 1;
+                $this->getManager()->rollbackMigrations($steps);
+                break;
+
+            case 'up':
+                if ($argc < 3) {
+                    echo "Erro: Nome da migration não fornecido.\n";
+                    exit(1);
+                }
+                $migrationName = $argv[2];
+                $this->getManager()->up($migrationName);
+                break;
+
+            case 'down':
+                if ($argc < 3) {
+                    echo "Erro: Nome da migration não fornecido.\n";
+                    exit(1);
+                }
+                $migrationName = $argv[2];
+                $this->getManager()->down($migrationName);
+                break;
+
+            default:
+                echo "Comando desconhecido: $this->command\n";
+                exit(1);
+        }
+    }
+
+    protected function getManager()
+    {
+        if (!$this->manager) {
+            $this->manager = new PdoMigrationManager(Main::getPdoStorage(), $this->migrationsDir);
+        }
+
+        return $this->manager;
+    }
+}
