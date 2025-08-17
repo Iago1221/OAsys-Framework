1df8c55cf642953fc14dd07ba9f0c47231b91565
Iago <prog.iago.oliveira@gmail.com>
2025-08-17 15:33:41 -0300

    Not found

--- src/Infrastructure/MVC/View/Layout/Base.php
+++ src/Infrastructure/MVC/View/Layout/Base.php
@@ -45,11 +45,16 @@ class Base implements ILayout
 
     private function renderHead()
     {
+        $sBase = '/assets/web-components';
+        $sUrl = General::$URL;
+
         ?>
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>OASYS</title>
+            <link rel="stylesheet" href="<?= $sBase ?>/css/styles.css">
+            <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.6.0/uicons-thin-rounded/css/uicons-thin-rounded.css'>
         </head>
         <?php
     }
@@ -102,8 +107,6 @@ class Base implements ILayout
         <script src="$sBase/js/Components/ConfirmComponent.js"></script>
         <script src="$sBase/js/app.js"></script>
         <script>App.getInstance().sUrl = '$sUrl'</script>
-        <link rel="stylesheet" href="$sBase/css/styles.css">
-        <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.6.0/uicons-thin-rounded/css/uicons-thin-rounded.css'>
         HTML;
     }
 }
